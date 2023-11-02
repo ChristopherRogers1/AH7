8344737718fa88e2f298a55167b64053c7cb515d
ChristopherRogers1 <60368736+ChristopherRogers1@users.noreply.github.com>
2023-11-02 13:08:31 -0400

    Update Source.java

--- Source.java
+++ Source.java
@@ -1,34 +1,55 @@
 class Node<T> {
-  T val;
-  Node<T> next;
+    T val;
+    Node<T> next;
 
-  public Node(T val) {
-    this.val = val;
-    this.next = null;
-  }
+    public Node(T val) {
+        this.val = val;
+        this.next = null;
+    }
 }
 
 class Source {
-  public static int sumList(Node<Integer> head) {
-    // todo
-    return;
-  }
+    public static int sumList(Node<Integer> head) {
+        int result=0;
+        Node<Integer> test = head;
+        while (test.next!=null)
+        {
+            result+=test.val;
+            test=test.next;
+        }
+        result+=test.val;
+        return result;
+    }
 
-  public static void main(String[] args) {
-    Node<Integer> a = new Node<>(2);
-    Node<Integer> b = new Node<>(8);
-    Node<Integer> c = new Node<>(3);
-    Node<Integer> d = new Node<>(-1);
-    Node<Integer> e = new Node<>(7);
+    public static void main(String[] args) {
+        Node<Integer> a = new Node<>(2);
+        Node<Integer> b = new Node<>(8);
+        Node<Integer> c = new Node<>(3);
+        Node<Integer> d = new Node<>(-1);
+        Node<Integer> e = new Node<>(7);
 
-    a.next = b;
-    b.next = c;
-    c.next = d;
-    d.next = e;
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
 
-    // 2 -> 8 -> 3 -> -1 -> 7
-    
-    System.out.println("Sum of my list: ");
-    System.out.println(Source.sumList(a));
-  }
+        Node<Integer> z = new Node<>(5);
+        Node<Integer> y = new Node<>(3);
+        Node<Integer> x = new Node<>(-3);
+        Node<Integer> w = new Node<>(0);
+        Node<Integer> v = new Node<>(8);
+
+        z.next = y;
+        y.next = x;
+        x.next = w;
+        w.next = v;
+
+        // 2 -> 8 -> 3 -> -1 -> 7
+
+        System.out.println("Sum of my list: ");
+        System.out.println(Source.sumList(a));
+
+        System.out.println("Sum of my list 2: ");
+        System.out.println(Source.sumList(z));
+    }
 }
